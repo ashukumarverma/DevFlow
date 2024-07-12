47f5b8c42de9738c1179bc1974a53dc2ab883f5a
Ashutosh <ashukumar3436@gmail.com>
2024-07-12 16:42:11 +0530

    ask question

--- database/question.model.ts
+++ database/question.model.ts
@@ -12,18 +12,18 @@ export interface IQuestion extends Document {
     createdAt: Date;
 }
 
-const QuestionSchema = new Schema<IQuestion>({
+const QuestionSchema = new Schema({
     title: { type: String, required: true },
     content: { type: String, required: true },
-    tags: [{ type: Schema.Types.ObjectId, ref: "Tag" }],
+    tags: [{ type: Schema.Types.ObjectId, ref: 'Tag' }],
     views: { type: Number, default: 0 },
-    upvotes: [{ type: Schema.Types.ObjectId, ref: "User" }],
-    downvotes: [{ type: Schema.Types.ObjectId, ref: "User" }],
-    author: { types: Schema.Types.ObjectId, ref: "User" },
-    answers: [{ type: Schema.Types.ObjectId, ref: "Answer" }],
+    upvotes: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    downvotes: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    author: { type: Schema.Types.ObjectId, ref: 'User' },
+    answers: [{ type: Schema.Types.ObjectId, ref: 'Answer' }],
     createdAt: { type: Date, default: Date.now }
 })
 
-const Question = models.Question || model("Question", QuestionSchema);
+const Question = models.Question || model('Question', QuestionSchema);
 
 export default Question;
--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -1,12 +1,40 @@
 'use server'
 
+import Question from "@/database/question.model";
 import { connectToDatabase } from "../mongoose"
+import Tag from "@/database/tag.model";
 
 
 export async function createQuestion(params: any) {
     // eslint-disable-next-line
     try {
         connectToDatabase();
+
+        const { title, content, tags, author, path } = params;
+
+        // Create a new question
+        const question = await Question.create({
+            title,
+            content,
+            author
+        });
+
+        const tagDocuments = [];
+
+        for (const tag of tags) {
+            const existingTag = await Tag.findOneAndUpdate({ name: { $regex: new RegExp(`^${tag}$`, "i") } },
+                { $setOnInsert: { name: tag }, $push: { questions: question._id } },
+                { upsert: true, new: true }
+            )
+            tagDocuments.push(existingTag._id);
+        }
+        await Question.findByIdAndUpdate(question._id, {
+            $push: { tags: { $each: tagDocuments } }
+        });
+
+        // Create an interaction reord for the user's ask_question action
+
+        // Icrement author's reputaoion by +5 for asking a question
     }
     catch (error) {
         // handle error
